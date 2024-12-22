ba62884fdc8ab8cb0055be649be38b73c3b054df
Zimraan Anjum <zimraan2012@gmail.com>
2024-12-22 22:11:47 +1100

    Created file Structure for frontend

---
+++ client/src/LandingPage.js
@@ -0,0 +1,14 @@
+import './LandingPage.css';  // Add this at the top of your file to link the styles
+
+function LandingPage() {
+    return (
+        <div className="App">
+            <h1 className='Title'>Landing Page</h1>
+            <p>This is the landing page</p>
+            <p>Follow the wireframe mockup i created and have fun :)</p>
+
+        </div>
+    );
+}
+
+export default LandingPage;
